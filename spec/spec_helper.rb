444791688163ff87381db63d86d234eb4b339f72
Ryan Travitz <rtravitz@gmail.com>
2016-12-12 17:31:57 -0700

    Add RSpec, Capybara, Factory Girl, and Database Cleaner

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'capybara/rspec'
+
+DatabaseCleaner[:active_record].strategy = :truncation
+RSpec.configure do |config|
+  config.before(:all) do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
