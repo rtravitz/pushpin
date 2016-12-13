b2fa3b6d568e36100d441e932dc99a99c9834069
Ryan Travitz <rtravitz@gmail.com>
2016-12-13 16:51:18 -0700

    Add role and user role. Add relationships to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213234456) do
+ActiveRecord::Schema.define(version: 20161213234843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_user_roles_on_role_id", using: :btree
+    t.index ["user_id"], name: "index_user_roles_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "status"
@@ -27,4 +42,6 @@ ActiveRecord::Schema.define(version: 20161213234456) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
