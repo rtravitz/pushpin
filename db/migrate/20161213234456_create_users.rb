791c8858689d52479e365e5985b795c99cdf1ed5
Ryan Travitz <rtravitz@gmail.com>
2016-12-13 16:47:07 -0700

    Add user model and migration

---
+++ db/migrate/20161213234456_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :status
+      t.string :name
+      t.string :username
+      t.string :location
+      t.string :email
+      t.string :phone
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    status "MyString"
+    name "MyString"
+    username "MyString"
+    location "MyString"
+    email "MyString"
+    phone "MyString"
+    password_digest "MyString"
+  end
+end
