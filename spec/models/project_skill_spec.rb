9a71fed304521c06fff3a3198659866c63479895
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-13 19:03:09 -0700

    Project Skill relationships

--- spec/models/project_skill_spec.rb
+++ spec/models/project_skill_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe ProjectSkill, type: :model do
+  context "relationships" do
+    it "responds to skill" do
+      skill = Skill.create(name: "Typer")
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(user: user, name: "Name", status: "unassigned")
+      project_skill = ProjectSkill.create(project: project, skill: skill)
+
+      expect(project_skill).to respond_to(:skill)
+    end
+
+    it "responds to project" do
+      skill = Skill.create(name: "Typer")
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(user: user, name: "Name", status: "unassigned")
+      project_skill = ProjectSkill.create(project: project, skill: skill)
+
+      expect(project_skill).to respond_to(:project)
+    end
+
+  end
 end
