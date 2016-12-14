8a32a9656e50c89b681b0639d8520eed3d121b11
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-13 19:42:12 -0700

    Proposal has many relationship tests

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -18,6 +18,24 @@ RSpec.describe Proposal, type: :model do
       expect(proposal).to respond_to(:user)
     end
 
+    it "responds to messages" do
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(name: "Name", user: user, status: "active")
+      proposal = Proposal.create(project: project, user: user)
+      message = Message.create(user: user, proposal: proposal, image_url: "http", body: "body of text")
+
+      expect(proposal).to respond_to(:messages)
+    end
+
+    it "responds to users" do
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(name: "Name", user: user, status: "active")
+      proposal = Proposal.create(project: project, user: user)
+      message = Message.create(user: user, proposal: proposal, image_url: "http", body: "body of text")
+
+      expect(proposal).to respond_to(:users)
+    end
+
 
   end
 end
