a562abc044543bac8861a9cbe03e40a52946d889
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-13 19:21:15 -0700

    Add Project relationship tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,10 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :proposals
+  has_many :users, through: :proposals
+  has_many :messages, through: :proposals
+  has_many :project_skills
+  has_many :skills, through: :project_skills
 
   validates :name, presence: true
   validates :user, presence: true
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -25,5 +25,62 @@ RSpec.describe Project, type: :model do
     end
   end
 
-  
+  context "relationships" do
+    it "responds to user" do
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.new(name: "Name", user: user, status: "active")
+
+      expect(project).to respond_to(:user)
+    end
+
+    it "responds to proposals" do
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(name: "Name", user: user, status: "active")
+      proposal = Proposal.create(project: project, user: user)
+
+      expect(project).to respond_to(:proposals)
+    end
+
+    it "responds to users" do
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(name: "Name", user: user, status: "active")
+      proposal = Proposal.create(project: project, user: user)
+
+      expect(project).to respond_to(:users)
+    end
+
+    it "responds to messages" do
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(name: "Name", user: user, status: "active")
+      proposal = Proposal.create(project: project, user: user)
+      message = Message.create(user: user, proposal: proposal, image_url: "http", body: "body of text")
+
+      expect(project).to respond_to(:messages)
+    end
+
+    it "responds to project_skills" do
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(name: "Name", user: user, status: "active")
+      skill = Skill.create(name: "Skill")
+      project_skill = ProjectSkill.create(project: project, skill: skill)
+
+      expect(project).to respond_to(:project_skills)
+    end
+
+    it "responds to skills" do
+      user = User.create(status: "active", name: "Name", username: "Uname", location: "Place", email: "e@email.com", phone: "12345667890", password_digest: "password")
+      project = Project.create(name: "Name", user: user, status: "active")
+      skill = Skill.create(name: "Skill")
+      project_skill = ProjectSkill.create(project: project, skill: skill)
+
+      expect(project).to respond_to(:skills)
+    end
+
+
+
+
+
+
+
+  end
 end
