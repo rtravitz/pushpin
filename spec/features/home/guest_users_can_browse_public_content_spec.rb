f73141da0a49023153a2a6239d6efafe7d15b5e4
Ryan Travitz <rtravitz@gmail.com>
2016-12-13 19:39:00 -0700

    Add factory for skill and test for browsing public skills

--- spec/features/home/guest_users_can_browse_public_content_spec.rb
+++ spec/features/home/guest_users_can_browse_public_content_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 feature "Guest user visits the root" do
   scenario "they can see options to select a needed service" do
-    
+    create_list(:skill, 4)
+    visit root_path
+
+    expect(page).to have_content("bricklaying")
+    expect(page).to have_content("snuggling")
   end
 
-  scenario "they click on 'Browse Professionals' and see an index of professionals" do
-    visit root_path
-    click_link "Browse Professionals"
-    
-  end
 end
