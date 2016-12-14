ad075cafa5654bc718ce32ec062a26965890f386
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-13 18:44:07 -0700

    Validation test skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,8 @@
 class Skill < ApplicationRecord
+  has_many :user_skills
+  has_many :users, through: :user_skills
+  has_many :projects, through: :project_skills
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
+  context "validations" do
+    it "is invalid without a name" do
+      skill = Skill.create()
+
+      expect(skill).to_not be_valid
+    end
+  end
+
+  context 
 end
