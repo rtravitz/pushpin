4bd4e6d5dc74fe8e9913c15dddede187a0916af1
Ryan Travitz <rtravitz@gmail.com>
2016-12-15 16:23:01 -0700

    Add basic form without relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214222941) do
+ActiveRecord::Schema.define(version: 20161215222121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20161214222941) do
     t.string   "status",     default: "unassigned"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.string   "location"
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
--- spec/features/requesters/requester_can_publicly_post_projects_spec.rb
+++ spec/features/requesters/requester_can_publicly_post_projects_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe "As a requester" do
   it "I can post projects from my dashboard" do
     user = create(:user)
+    skill1, skill2 = create_list(:skill, 2)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit requester_dashboard_path(user)
 
-    click_button("Create New Project")
-    expect(page).to have_current_path("/#{user.username}/projects/new")
+    click_link("Create New Project")
+    expect(page).to have_current_path("/requester/projects/new")
 
-    fill_in "name", with: "Fix Bathroom"
-    fill_in "location", with: "1510 Blake Street"
-    fill_in "skill", with: "Plumbing"
+    fill_in "project_name", with: "Fix Bathroom"
+    fill_in "project_location", with: "1510 Blake Street"
     click_button("Create Project")
 
     expect(page).to have_current_path(requester_dashboard_path)
