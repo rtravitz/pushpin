bc19f769a5b7ed76a11eefe8f79a21513d310db8
alishersadikov <alishersadikov@gmail.com>
2016-12-15 10:59:32 -0700

    setup the spec

---
+++ spec/features/admin/admin_authorization_test_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature "Only admin should be able to view admin dashboard" do
+  scenario "a requester visits admin dashboard" do
+    requester_role = create(:role, title: "requester")
+    requester = create(:user)
+    requester.roles << requester_role
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(requester)
+
+    visit "/admin/dashboard/#{requester.id}"
+
+    expect(current_path).to eq(admin_dashboard_path(requester))
+
+  end
+end
