46766c93d5cd63628b196889e9e644b8879796f0
Alisher Sadikov <alishersadikov@gmailcom>
2016-12-14 17:46:57 -0700

    activate/deactivate functionality complete

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,12 @@ class Admin::DashboardController < ApplicationController
   end
 
   def update_user
-    binding.pry
+    user = User.find(params[:id])
+    if user.status == "active"
+      user.update_attributes(status: "inactive")
+    else
+      user.update_attributes(status: "active")
+    end
+    redirect_to admin_dashboard_path(current_user)
   end
 end
