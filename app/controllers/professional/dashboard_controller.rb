b752ac42befe693488d38a80857bec394f4fb7f6
Megan Talbot <meganftalbot@gmail.com>
2016-12-17 13:35:26 -0700

    Add @possible_projects in dashboard controller for professionals

--- app/controllers/professional/dashboard_controller.rb
+++ app/controllers/professional/dashboard_controller.rb
@@ -2,7 +2,6 @@ class Professional::DashboardController < ApplicationController
   def show
     @professional = current_user
     @skills = Skill.all
-
     @possible_projects = Project.joins(skills: :users).where("user_skills.user_id = #{current_user.id}")
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -8,16 +8,6 @@ class Project < ApplicationRecord
 
   validates :name, presence: true
   validates :user, presence: true
-
-
-  # def self.possible_projects(user_id)
-  #
-  #   user = User.find(user_id)
-  #   user.skills.each do |skill|
-  #     skill.projects
-  #   end
-  # end
-
   validates :slug, uniqueness: true
 
   before_validation :generate_slug
