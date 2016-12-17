638c65aa6c8c196c92f8a451e0e9073c0b9e3ab4
Ryan Travitz <rtravitz@gmail.com>
2016-12-17 12:01:52 -0700

    Add slug to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216013500) do
+ActiveRecord::Schema.define(version: 20161217190107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20161216013500) do
     t.datetime "updated_at",                         null: false
     t.string   "location"
     t.text     "description"
+    t.string   "slug"
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
