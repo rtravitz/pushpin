91dda4bbbfaa421ec539ecc99d41d1ed157d00cd
Megan Talbot <meganftalbot@gmail.com>
2016-12-17 16:03:21 -0700

    Add test for professionals seeing potential projects index

--- app/controllers/professional/dashboard_controller.rb
+++ app/controllers/professional/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Professional::DashboardController < ApplicationController
   def show
     @professional = current_user
     @skills = Skill.all
-    @possible_projects = Project.joins(skills: :users).where("user_skills.user_id = #{current_user.id}")
+    @possible_projects = User.joins(skills: :users).where("user_skills.user_id = #{current_user.id}")
   end
 end
