c1d127c42cca531ce3d19608892ad58b178b094c
Ryan Travitz <rtravitz@gmail.com>
2016-12-17 12:52:25 -0700

    Add route and method in controller

--- spec/features/requesters/requester_can_visit_their_existing_project_page_spec.rb
+++ spec/features/requesters/requester_can_visit_their_existing_project_page_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 feature "As a requester when I visit my existing project page" do
   scenario "I can see the information for my project" do
     user = create(:user)
+    requester, professional, admin = create_list(:role, 3)
     project = create(:project)
+    user.roles << requester
     user.projects << project
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit requester_project_path(project: project.slug)
 
