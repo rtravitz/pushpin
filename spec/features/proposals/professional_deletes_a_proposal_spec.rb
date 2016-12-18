2a9c9a4dc7352c0601fb404727f3c608776fa5dc
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-17 19:34:30 -0700

    Create Proposal factory

---
+++ spec/features/proposals/professional_deletes_a_proposal_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+describe "professional deletes a proposal" do
+  context "delete a proposal from professional dashboard" do
+    it "removes the proposal" do
+      
+    end
+  end
+end
