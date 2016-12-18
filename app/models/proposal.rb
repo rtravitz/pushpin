2029ad58faa77b9ce69b93b0db1f1058e5b92790
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-17 20:43:25 -0700

    Delete proposals working in live environment but not test

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,6 +2,6 @@ class Proposal < ApplicationRecord
   belongs_to :project
   belongs_to :user
 
-  has_many :messages
-  has_many :users, through: :messages
+  has_many :messages, dependent: :destroy
+  has_many :users, through: :messages, dependent: :destroy
 end
