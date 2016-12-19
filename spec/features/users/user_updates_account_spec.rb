bade58395d193acc5ade8b8ebc1347036ad35d2d
alishersadikov <alishersadikov@gmail.com>
2016-12-18 18:13:00 -0700

    complete user update account info and add role functionality with test

--- spec/features/users/user_updates_account_spec.rb
+++ spec/features/users/user_updates_account_spec.rb
@@ -21,22 +21,78 @@ describe "User updates account information" do
     end
   end
 
-  context "user updates account information" do
-    it "updated information is stored" do
-      user = create(:user)
-      requester_role = create(:role, title: "requester")
-      user.roles << requester_role
+  context "user goes to user show page" do
+    let(:user) { create(:user, name: "jeff") }
+    let(:requester_role) { create(:role, title: "requester") }
 
+    it "user can see all existing account information" do
+      user.roles << requester_role
       login(user)
 
       visit user_path(user)
 
+      expect(page).to have_content(user.name)
+      expect(page).to have_content(user.username)
+      expect(page).to have_content(user.email)
+      expect(page).to have_content(user.location)
+      expect(page).to have_content(user.phone)
+      expect(page).to have_link("Update account info")
+    end
 
+    it "user updates information" do
+      user.roles << requester_role
+      login(user)
 
-      save_and_open_page
+      visit user_path(user)
 
+      expect(page).to have_content("jeff")
 
+      click_link "Update account info"
+
+      expect(current_path).to eq(edit_user_path(user))
+
+      fill_in "user[name]", with: "new name"
+
+      click_on "Update"
+
+      expect(current_path).to eq(user_path(user))
+      expect(page).not_to have_content("jeff")
+      expect(page).to have_content("new name")
+    end
+
+    it "requester user adds a professional role" do
+      user.roles << requester_role
+      create(:role, title: "professional")
+      login(user)
+
+      visit edit_user_path(user)
+
+      select "professional", :from => "user[role_to_add]"
+
+      click_on "Update"
+
+      expect(current_path).to eq(user_path(user))
+      expect(User.last.roles.count).to eq(2)
+      expect(User.last.roles.first).to eq(Role.find_by(title: "requester"))
+      expect(User.last.roles.last).to eq(Role.find_by(title: "professional"))
+    end
+
+    it "professional user adds a requester role" do
+      prof_role = create(:role, title: "professional")
+      user.roles << prof_role
+      requester_role #calls the above let block
+      login(user)
+
+      visit edit_user_path(user)
+
+      select "requester", :from => "user[role_to_add]"
+
+      click_on "Update"
+
+      expect(current_path).to eq(user_path(user))
+      expect(User.last.roles.count).to eq(2)
+      expect(User.last.roles.first).to eq(Role.find_by(title: "professional"))
+      expect(User.last.roles.last).to eq(Role.find_by(title: "requester"))
     end
   end
-
 end
