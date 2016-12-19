433d32a8cd81c230315f1430de562f2a064bb8c7
alishersadikov <alishersadikov@gmail.com>
2016-12-18 21:35:55 -0700

    some minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
+    @user = only_current_user
   end
 
   def update
@@ -42,10 +42,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :username, :status, :email, :location, :phone, :password, :password_confirmation)
     end
 
-    # below method was added to account for the edge case
-    # when users try to go to someone else's page,
-    # they can only see their own info regardless
-    # but this stops them from even trying
     def only_current_user
       if params[:id].to_i == current_user.id
         current_user
