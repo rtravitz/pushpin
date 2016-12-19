de3e65403ac111a50c588aa6db37dd5bd1a436b0
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-18 17:18:59 -0700

    Refactor professional proposal show test

--- spec/features/proposals/professional_sees_their_proposal_show_spec.rb
+++ spec/features/proposals/professional_sees_their_proposal_show_spec.rb
@@ -1,24 +1,30 @@
 require "rails_helper"
 
 describe "professional visits a proposal" do
+  before :each do
+    role = create(:role, title: "professional")
+    user = create(:user)
+    user.roles << role
+    @proposal = create(:proposal, user: user)
+    login(@proposal.user)
+  end
   context "sees a proposal show professional dashboard" do
     it "sees the proposal" do
-      role = create(:role, title: "professional")
-      user = create(:user)
-      user.roles << role
-      proposal = create(:proposal, user: user)
-      login(proposal.user)
 
-      visit professional_dashboard_path(proposal.user)
-      within(".proposal-#{proposal.id}") do
-        click_on "#{proposal.id}"
+      visit professional_dashboard_path(@proposal.user)
+      within(".proposal-#{@proposal.id}") do
+        click_on "#{@proposal.id}"
       end
 
-      expect(current_path).to eq(professional_proposal_path(proposal))
-      expect(page).to have_content("Proposal ID: #{proposal.id}")
-      expect(page).to have_content("Project Name: #{proposal.project.name}")
-      expect(page).to have_content("Project Requester: #{proposal.project.user.name}")
+      expect(current_path).to eq(professional_proposal_path(@proposal))
+      expect(page).to have_content("Proposal ID: #{@proposal.id}")
+      expect(page).to have_content("Project Name: #{@proposal.project.name}")
+      expect(page).to have_content("Project Requester: #{@proposal.project.user.name}")
       expect(page).to have_content("Message Center")
     end
   end
+
+  # context "professional can link to requester from proposal show" do
+  #
+  # end
 end
