e43274184fd514de1d55af3f80962b1cb048a742
Ryan Travitz <rtravitz@gmail.com>
2016-12-19 15:35:15 -0700

    Add test for professional sending messages

--- spec/features/messages/users_can_send_messages_on_proposals_spec.rb
+++ spec/features/messages/users_can_send_messages_on_proposals_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "A user can send messages on a proposal" do
-  context "When a user visits a proposal" do
+  context "When a requester visits a proposal" do
     scenario "they can send a message" do
       user1, user2 = create_list(:user, 2)
       requester, professional= create_list(:role, 2)
@@ -13,6 +13,26 @@ feature "A user can send messages on a proposal" do
 
       visit requester_proposal_path(proposal)
       message = "Would you like some iced tea, dearie?"
+
+      fill_in "message_body", with: message
+      click_button "Send"
+
+      expect(page).to have_content(message)
+    end
+  end
+
+  context "When a requester visits a proposal" do
+    scenario "they can send a message" do
+      user1, user2 = create_list(:user, 2)
+      requester, professional= create_list(:role, 2)
+      user1.roles << requester
+      user2.roles << professional
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user2)
+      project = create(:project, user: user1)
+      proposal = create(:proposal, user: user2, project: project)
+
+      visit professional_proposal_path(proposal)
+      message = "No thanks."
 
       fill_in "message_body", with: message
       click_button "Send"
