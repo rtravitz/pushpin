1c7263c70cf83696124260b040a3c8a2fcac883e
Ryan Travitz <rtravitz@gmail.com>
2016-12-20 14:43:30 -0700

    Add pdf icon and support for pdf uploads

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def image_or_pdf_icon(message)
+    if message.image_url[-4..-1] == ".pdf"
+      "/assets/pdf.ico" 
+    else
+      message.image_url
+    end
+  end
 end
