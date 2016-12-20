45b954b954f484ee564d4d7c696907b601a7d584
Ryan Travitz <rtravitz@gmail.com>
2016-12-20 15:07:31 -0700

    Add test for helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def image_or_pdf_icon(message)
-    if message.image_url[-4..-1] == ".pdf"
+  def image_or_pdf_icon(url)
+    if url[-4..-1] == ".pdf"
       image_tag("/assets/pdf.ico", class: "msg-pdf-icon")
     else
-      image_tag(message.image_url, class: "msg-img")
+      image_tag(url, class: "msg-img")
     end
   end
 end
