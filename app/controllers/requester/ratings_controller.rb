7a371bda9806307f2a8b20f451b91ff9977ca69d
alishersadikov <alishersadikov@gmail.com>
2016-12-19 17:22:22 -0700

    minor refactoring

--- app/controllers/requester/ratings_controller.rb
+++ app/controllers/requester/ratings_controller.rb
@@ -1,8 +1,14 @@
 class Requester::RatingsController < ApplicationController
 
   def create
-    @rating = Rating.create!(score: params[:rating][:score], user_id: params[:professional], giver_id: current_user.id)
-    redirect_to requester_dashboard_path(current_user)
+    rating = Rating.new(score: params[:rating][:score], user_id: params[:professional], giver_id: current_user.id)
+    if !rating.save
+      redirect_to requester_project_path(current_user)
+      flash[:warning] = "Something went wrong. Please try again."
+    else
+      redirect_to requester_dashboard_path(current_user)
+      flash[:success] = "Thanks for rating #{rating.user.name}!"
+    end
   end
 
   private
