c50c9f3cad1f274618a78e6a021aea5cb39f2887
Ryan Travitz <rtravitz@gmail.com>
2016-12-20 14:04:21 -0700

    Complete image message center

--- app/controllers/professional/proposals_controller.rb
+++ app/controllers/professional/proposals_controller.rb
@@ -1,4 +1,5 @@
 class Professional::ProposalsController < ApplicationController
+  before_action :set_s3_direct_post, only: [:show]
 
   def show
     @proposal = Proposal.find(params[:id])
@@ -34,6 +35,10 @@ class Professional::ProposalsController < ApplicationController
 
     def proposal_params
       params.require(:proposal).permit(:user_id, :project_id)
+    end
+    
+    def set_s3_direct_post
+      @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
     end
 
 end
