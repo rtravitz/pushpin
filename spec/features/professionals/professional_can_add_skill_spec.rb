c8730440048fecef42a79795ff6028b24f9d9e42
Megan Talbot <meganftalbot@gmail.com>
2016-12-20 20:38:38 -0700

    Pass test for professional user adding skill on dashboard

--- spec/features/professionals/professional_can_add_skill_spec.rb
+++ spec/features/professionals/professional_can_add_skill_spec.rb
@@ -4,6 +4,7 @@ feature "Professional user visits the site and adds skill" do
   context "professional visits the professional dashboard" do
     it "allows professional user to add a skill" do
       user = create(:user)
+      new_skill = create(:skill, name: "shoveling")
       user.skills << create(:skill)
       user.roles << create(:role, title: "professional")
 
@@ -11,8 +12,10 @@ feature "Professional user visits the site and adds skill" do
 
       visit professional_dashboard_path
 
-      # fill_in "New Skill", with: "Woodworking"
-      # click_on "Add a Skill"
+      within ".professional_add_skill" do
+        select "shoveling", from: "skill[skill_id]"
+        click_on "Add this skill!"
+      end
 
       expect(page).to have_content("Welcome to Your Professional Dashboard")
       expect(page).to have_content(user.skills.last.name)
