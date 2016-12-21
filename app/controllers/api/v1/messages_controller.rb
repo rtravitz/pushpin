a5f64068394dca3da4462e380c019faf0ab309cc
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-21 12:01:57 -0700

    Update Post request to require json

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::MessagesController < ApplicationController
+class Api::V1::MessagesController < ApiController
 
   def index
     messages = Message.by_proposal(params[:proposal])
@@ -8,8 +8,14 @@ class Api::V1::MessagesController < ApplicationController
   def create
     proposal = Proposal.find(params[:proposal])
     user = User.find_by(api_key: params[:api_key])
-    message = proposal.messages.create(body: params[:message], user: user, image_url: params[:file])
+    message = proposal.messages.create(body: params[:message][:body], image_url: params[:message][:file], user_id: user)
     render json: message
   end
+
+  private
+
+  # def message_params
+  #   params.require(:message).permit(message: [:body, :image_url])
+  # end
 
 end
