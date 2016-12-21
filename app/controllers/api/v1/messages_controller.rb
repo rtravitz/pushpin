c2111b88bf7a6308d4a7bb561082b33b05273227
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-20 20:33:28 -0700

    Create message from post request

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -5,4 +5,11 @@ class Api::V1::MessagesController < ApplicationController
     render json: messages
   end
 
+  def create
+    proposal = Proposal.find(params[:proposal])
+    user = User.find_by(api_key: params[:api_key])
+    message = proposal.messages.create(body: params[:message], user: user, image_url: params[:file])
+    render json: message
+  end
+
 end
