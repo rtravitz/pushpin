e0261c45a69f371aefc7a0a33f6eec322caf5e22
Ryan Travitz <rtravitz@gmail.com>
2016-12-21 10:51:10 -0700

    Add test for message center transferring to an accepted project page

---
+++ spec/features/messages/user_sees_messages_from_accepted_proposal_on_project_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature "As a professional with an accepted proposal for a project" do
+  scenario "my message history transfers to the project" do
+    user1, user2 = create_list(:user, 2)
+    requester = create(:role, title: "requester")
+    professional = create(:role, title: "professional")
+    user1.roles << requester
+    user2.roles << professional
+    login(user2)
+    project = create(:project, user: user1)
+    proposal = create(:proposal, user: user2, project: project)
+    message = proposal.messages.create(body: "test body", user: user1)
+
+    visit professional_project_path(project: project.slug)
+
+    expect(page).to_not have_content(message.body)
+
+    project.update_attributes(status: "accepted")
+    proposal.update_attributes(status: "accepted")
+
+    visit professional_project_path(project: project.slug)
+
+    expect(page).to have_content("Message Center")
+    expect(page).to have_content(message.body)
+  end
+end
