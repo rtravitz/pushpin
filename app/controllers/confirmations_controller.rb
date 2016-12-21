79e124a31224bff8a4fb444dc532cfa57b58994b
Megan Talbot <meganftalbot@gmail.com>
2016-12-21 15:57:56 -0700

    Update twilio two functionality, row roots back to dashboard page

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,22 +1,18 @@
 class ConfirmationsController < ApplicationController
   def new
     @user = current_user
-    redirect_to user_confirmation_path
+    ConfirmationSender.send_confirmation_to(@user)
+
   end
 
   def update
     @user = current_user
-    ConfirmationSender.send_confirmation_to(@user)
-    # redirect_to user_confirmation_path
-
-    binding.pry
     if @user.verification_code == params[:user][:verification_code]
-
       session[:authenticated] = true
       @user.update_attributes(password: params[:password])
 
       flash[:notice] = "You have successfully updated your password!"
-      redirect_to root_path
+      redirect_to "/#{@user.roles.first.title}/dashboard"
     else
       flash.now[:error] = "Verification code is incorrect."
       render :new
