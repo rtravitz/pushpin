bff48767dbd8b56688ecd283555cc2d2fb89dc81
Alisher Sadikov <alishersadikov@gmail.com>
2016-12-21 12:46:01 -0700

    create strong params in api message controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -8,14 +8,27 @@ class Api::V1::MessagesController < ApiController
   def create
     proposal = Proposal.find(params[:proposal])
     user = User.find_by(api_key: params[:api_key])
-    message = proposal.messages.create(body: params[:message][:body], image_url: params[:message][:file], user_id: user)
+    binding.pry
+    message = proposal.messages.create(message_params(user))
     render json: message
   end
 
   private
 
-  # def message_params
-  #   params.require(:message).permit(message: [:body, :image_url])
+  def message_params(user)
+    params.permit(:message).tap do |whitelisted|
+      whitelisted[:body] = params[:message][:body]
+      whitelisted[:image_url] = params[:message][:image_url]
+      whitelisted[:user_id] = user.id
+    end
+  end
+  #
+  #   permit(:message).tap do |whitelisted|
+  #     whitelisted[:body]
+  #
+  #      [:body, :image_url])
   # end
-
+  # params.require(:measurement).permit, :message).tap do |whitelisted|
+  #     whitelisted[:message] = params[:message]
+  #
 end
