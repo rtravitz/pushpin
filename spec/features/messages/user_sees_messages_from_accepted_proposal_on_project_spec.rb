8c42c1ca7b2b4213edd0f4125a2d8cc5ec03fcb4
Ryan Travitz <rtravitz@gmail.com>
2016-12-21 11:36:55 -0700

    Pass spec for requester seeing project show page

--- spec/features/messages/user_sees_messages_from_accepted_proposal_on_project_spec.rb
+++ spec/features/messages/user_sees_messages_from_accepted_proposal_on_project_spec.rb
@@ -1,27 +1,55 @@
 require 'rails_helper'
 
-feature "As a professional with an accepted proposal for a project" do
-  scenario "my message history transfers to the project" do
-    user1, user2 = create_list(:user, 2)
-    requester = create(:role, title: "requester")
-    professional = create(:role, title: "professional")
-    user1.roles << requester
-    user2.roles << professional
-    login(user2)
-    project = create(:project, user: user1)
-    proposal = create(:proposal, user: user2, project: project)
-    message = proposal.messages.create(body: "test body", user: user1)
+feature "As a user with an accepted proposal for a project" do
+  context "who is a professional" do
+    scenario "my message history transfers to the project" do
+      user1, user2 = create_list(:user, 2)
+      requester = create(:role, title: "requester")
+      professional = create(:role, title: "professional")
+      user1.roles << requester
+      user2.roles << professional
+      login(user2)
+      project = create(:project, user: user1)
+      proposal = create(:proposal, user: user2, project: project)
+      message = proposal.messages.create(body: "test body", user: user1)
 
-    visit professional_project_path(project: project.slug)
+      visit professional_project_path(project: project.slug)
 
-    expect(page).to_not have_content(message.body)
+      expect(page).to_not have_content(message.body)
 
-    project.update_attributes(status: "assigned")
-    proposal.update_attributes(status: "assigned")
+      project.update_attributes(status: "assigned")
+      proposal.update_attributes(status: "assigned")
 
-    visit professional_project_path(project: project.slug)
+      visit professional_project_path(project: project.slug)
 
-    expect(page).to have_content("Message Center")
-    expect(page).to have_content(message.body)
+      expect(page).to have_content("Message Center")
+      expect(page).to have_content(message.body)
+    end
+  end
+
+  context "who is a requester" do
+    scenario "my message history transfers to the project" do
+      user1, user2 = create_list(:user, 2)
+      requester = create(:role, title: "requester")
+      professional = create(:role, title: "professional")
+      user1.roles << requester
+      user2.roles << professional
+      login(user1)
+      project = create(:project, user: user1)
+      proposal = create(:proposal, user: user2, project: project)
+      message = proposal.messages.create(body: "test body", user: user1)
+
+      visit requester_project_path(project: project.slug)
+
+      expect(page).to_not have_content(message.body)
+
+      project.update_attributes(status: "assigned")
+      proposal.update_attributes(status: "assigned")
+
+      visit requester_project_path(project: project.slug)
+
+      expect(page).to have_content("Message Center")
+      expect(page).to have_content(message.body)
+    end
   end
 end
